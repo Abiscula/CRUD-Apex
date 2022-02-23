067e8cab4c4ba2f54c8a3fca3b469480c146082a
Abiscula <victor.augusto@bcrservicos.com.br>
2022-02-23 11:27:23 -0300

    criado conexão com banco de dados

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,22 @@
+require('dotenv/config')
+const Knex = require('knex')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+
+const knexClient = Knex({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        port: 3306,
+        user: process.env['DB_USER'],
+        password: process.env['DB_PASSW'],
+        database: process.env['DB_DATABASE']
+    }
+})
+
+console.log(knexClient)
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -21,4 +36,9 @@ app.post('/login', (req, res) => {
     } else {
         res.send('Usuário ou senha inválidos')
     }
+})
+
+app.post('/register', (req, res) => {
+    const { user, passw, nick } = req.body
+    
 })
